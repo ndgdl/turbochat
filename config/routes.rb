4ba9cf7a6acea9bdb42b331411434b298de01d00
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-02-22 11:01:12 +0100

    sending messages to a specific chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
   devise_scope :user do
     get 'users', to: 'devise/session#new'
   end
 
   get 'user/:id', to: 'user#show', as: 'user'
 
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
