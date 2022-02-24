46007cf941caab75af1309903404b3c8ceafbec8
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-02-24 18:08:12 +0100

    gravatar used for avatar, no image upload

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def gravatar_for(user, options = { size: 80, class: "gravatar" })
+    size         = options[:size]
+    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.email, class: "#{ options[:class] }")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,8 @@ class User < ApplicationRecord
   after_create_commit { broadcast_append_to 'users' }
   after_update_commit { broadcast_update }
   has_many :messages
-  has_one_attached :avatar
 
   enum status: %i[offline away online]
-
-  after_commit :add_default_avatar, on: %i[create update]
-
-  def avatar_thumbnail
-    avatar.variant(resize_to_limit: [150, 150]).processed
-  end
-
-  def chat_avatar
-    avatar.variant(resize_to_limit: [50, 50]).processed
-  end
 
   def broadcast_update
     broadcast_replace_to 'user_status', partial: 'users/status', user: self
@@ -33,16 +22,5 @@ class User < ApplicationRecord
     else
       'bg-dark'
     end
-  end
-
-  private
-
-  def add_default_avatar
-    return if avatar.attached?
-    avatar.attach(
-      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_avatar.jpg')),
-      filename: 'default_avatar.jpg',
-      content_type: 'image/jpg'
-    )
   end
 end
