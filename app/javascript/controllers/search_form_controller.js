6a83dbfa168c2c7826ce6222b4569e7282cae7e0
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-02-26 15:41:02 +0100

    prevented too many requests with stimulus

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -1,16 +1,27 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input", "results"]
+  static targets = ["form", "input", "results"]
 
-  displayResults() {
-    if (this.inputTarget.value === "") {
-      this.resultsTarget.classList.remove("d-none")
-    }
-    else {
-      this.resultsTarget.classList.remove("d-none")
-    }
+  connect() {
+    console.log("ça marche ?");
   }
+
+  search() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.formTarget.requestSubmit()
+    }, 500);
+  }
+
+  // displayResults() {
+  //   if (this.inputTarget.value === "") {
+  //     this.resultsTarget.classList.remove("d-none")
+  //   }
+  //   else {
+  //     this.resultsTarget.classList.remove("d-none")
+  //   }
+  // }
   // hideResults() {
   //   this.resultsTarget.classList.add("d-none")
   // }
